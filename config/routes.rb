6355e5c48709f626718c3ff3c90e88f7665e1061
Brian Liu <brian.b.liu@gmail.com>
2020-10-11 01:06:42 -0600

    Comment out get welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/', to: 'welcome#index'
+  # get '/', to: 'welcome#index'
 
 # /SHELTERS ROUTES
   get '/shelters', to: 'shelters#index'
